47a1cbc3af43b9757dd17bbfc1e5f14d594d0d85
Mauricio-Java <mcsouza.pinda@gmail.com>
2021-02-28 14:41:26 -0300

    Projeto criado

---
+++ src/main/java/com/example/Prep_for_test/PrepForTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prep_for_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepForTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepForTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Prep_for_test/PrepForTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prep_for_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepForTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
